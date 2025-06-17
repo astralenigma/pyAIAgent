1c2feee6427f8803e9e36242135080d6664e4d20
Astralenigma <astralenigma@hotmail.com>
2025-06-17 12:16:09 +0100

    Gemini plugged

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+verbose=False
+if len(sys.argv)<2:
+    print("Usage: python3 main.py <prompt>")
+    sys.exit(1)
+if len(sys.argv)>2:
+    if sys.argv[2]=="--verbose":
+        verbose=True
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
+user_prompt=sys.argv[1]
 client = genai.Client(api_key=api_key)
-
-content=client.models.generate_content(model='gemini-2.0-flash-001',
-    contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."])
-print(content.text)
-print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+            ]
+response=client.models.generate_content(model='gemini-2.0-flash-001',
+    contents=messages)
+print(response.text)
+if(verbose):
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
