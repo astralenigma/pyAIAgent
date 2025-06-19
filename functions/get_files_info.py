18e04fd5e214e6bf26e5cf73f8a9f7ce78f4ece3
Astralenigma <astralenigma@hotmail.com>
2025-06-19 01:56:11 +0100

    Added ability to read directory contents.

---
+++ functions/get_files_info.py
@@ -0,0 +1,20 @@
+import os
+def get_files_info(working_directory, directory=None):
+    abs_WD=os.path.abspath(working_directory)
+    if(directory.startswith("/")):
+        abs_TD=os.path.abspath(directory)
+    else:
+        if(directory.startswith(".")):
+            abs_TD=abs_WD
+        else:
+            abs_TD=os.path.join(abs_WD,directory)
+    
+    if(not abs_TD.startswith(abs_WD) or directory=="../"):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if(not os.path.isdir(abs_TD)):
+        return f'Error: "{directory}" is not a directory'
+    dir_details=""
+    for file in os.listdir(abs_TD):
+        tfile=os.path.join(abs_TD,file)
+        dir_details+=f"- {file}: file_size={os.path.getsize(tfile)} bytes, is_dir={os.path.isdir(tfile)}\n"
+    print(dir_details)
