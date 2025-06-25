73242506bcc8b579e819513fd39102e0023ae805
Astralenigma <astralenigma@hotmail.com>
2025-06-25 11:45:19 +0100

    Reestructuring for better code readability and processing

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import os
 import sys
-from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 # from subdirectory.filename import function_name
 from prompts import system_prompt
-from call_function import available_functions
-verbose=False
-if len(sys.argv)<2:
-    print("Usage: python3 main.py <prompt>")
-    sys.exit(1)
-if len(sys.argv)>2:
-    if sys.argv[2]=="--verbose":
-        verbose=True
+from call_function import available_functions,call_function
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-user_prompt=sys.argv[1]
-client = genai.Client(api_key=api_key)
-model_name='gemini-2.0-flash-001'
-
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+def main():
+    load_dotenv()
+    verbose= "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")] 
+    if not args:
+        print("Asssistente de código Artificial")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        user_prompt=" ".join(args)
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
             ]
-response=client.models.generate_content(model=model_name,
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+        generate_content(client,messages,verbose)
 
-print(response.text)
-if (response.function_calls):
+def generate_content(client, messages,verbose):
+    model_name='gemini-2.0-flash-001'
+    
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        ),
+    )
+    if(verbose):
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
+        return response.text
+    
     for function_call_part in response.function_calls:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        
-if(verbose):
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        call_function(function_call_part,verbose)
+
+
+
+
+
+if __name__=="__main__":
+    main()
